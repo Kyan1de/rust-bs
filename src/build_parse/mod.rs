4f233efa52c1246ccd0c4ba81bd288052ad2aa65
Kyan1de <k.beau1@outlook.com>
2025-10-04 20:14:36 -0600

    fully refactored the parser to use an actual token struct

--- src/build_parse/mod.rs
+++ src/build_parse/mod.rs
@@ -42,133 +42,134 @@ pub enum BSAst {
     ExprTerm(Vec<BSAst>) // (<expr>) || <Num||Ident||Str>
 }
 
+/// used to make tokens
+#[derive(Debug, PartialEq, Clone)]
+pub enum BSToken {
+    Operator(String), // single character things
+    String(String), // string literals
+    Numeric(String), // numeric literals
+    Word(String), // any other thing (keywords, identifiers)
+    Break, // break between lines, basically a \n
+}
 
 impl BuildParser {
 
     /// takes a string input and splits it into token strings
-    pub fn lex(input: &str) -> Vec<&str> {
+    pub fn lex(input: &str) -> Vec<BSToken> {
         let mut out = vec![];
 
         input.split("\n").for_each(|l|{
-            let mut split: Vec<&str> = Regex::new("(\\d+\\.\\d+)|(\\\".*?\\\")|(#.*)|[\\+\\-\\*\\/\\=\\(\\)\\[\\]\\,]|(\\b\\S+?\\b)").unwrap()
-                                    .find_iter(&l).map(|mat|{
-                                        if mat.as_str().ends_with("\r") {&mat.as_str()[..(mat.len()-1)]} else {mat.as_str()}
-                                    })
-                                    .collect();
-            out.append(&mut split);
-            out.push("\n");
+            let split: Vec<&str> = Regex::new("(\\d+\\.\\d+)|(\\\".*?\\\")|(#.*)|[\\+\\-\\*\\/\\=\\(\\)\\[\\]\\,]|(\\b\\S+?\\b)").unwrap()
+                                   .find_iter(&l).map(|mat|{
+                                       if mat.as_str().ends_with("\r") {&mat.as_str()[..(mat.len()-1)]} else {mat.as_str()}
+                                   })
+                                   .collect();
+            split.iter().for_each(|s| {
+                match s.chars().nth(0) {
+                    Some('+'|'-'|'*'|'/'|'='|'('|')'|'['|']'|',') => {out.push(BSToken::Operator(s.to_string()));},
+                    Some('0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9') => {out.push(BSToken::Numeric(s.to_string()));},
+                    Some('"') => {out.push(BSToken::String(s.to_string()));},
+                    None | Some('#') => {},
+                    _ => {out.push(BSToken::Word(s.to_string()));},
+                }
+            });
+            out.push(BSToken::Break);
         });
         
         out
     }
 
     /// takes a vector of token strings and constructs an AST
-    pub fn parse(input: Vec<&str>) -> BSAst {
-        
-        let mut clean: Vec<&str> = vec![];
-
-        for token in input.windows(2) {
-            match token {
-                [a, b] => {
-                    if (*a).eq("\n") && (*b).eq("\n") {
-                        () // dont push the token if followed immediately by another \n
-                    } else if !a.starts_with("#") {
-                        clean.push(a);
-                    }
-                },
-                [a] => {
-                    if (*a).eq("\n") {
-                        () // dont push the token if no tokens follow
-                    } else if !a.starts_with("#") {
-                        clean.push(a);
-                    }        
-                },
-                _ => {()}
-            }
-        }
-
-        BSAst::Prog(Self::parse_lines(&mut clean))
+    pub fn parse(input: Vec<BSToken>) -> BSAst {
+        BSAst::Prog(Self::parse_lines(&mut input.split(|t|*t==BSToken::Break)))
     }
 
     /// parses lines of tokens
-    fn parse_lines(global_toks: &mut Vec<&str>) -> Vec<BSAst>{
-        let mut statement: Vec<&str>;
+    fn parse_lines(global_toks: &mut (dyn Iterator<Item = &[BSToken]> + '_)) -> Vec<BSAst> {
+        
         let mut parsed = vec![];
+
         loop {
-            let idx = global_toks.iter().position(|a| (*a).eq("\n")).unwrap_or(0);
-            if idx == global_toks.len() {break;}
-            (statement, *global_toks) = {
-                let (a, b) = global_toks.split_at(idx);
-                let (_, b) = b.split_first().unwrap();
-                (Vec::from(a), Vec::from(b))
-            };
-            parsed.push(Self::parse_part(statement.as_slice(), global_toks));
+            let Some(statement) = global_toks.next() else {return parsed;};
+            parsed.push(Self::parse_part(&mut statement.iter().peekable(), global_toks));
         }
-        parsed
     }
 
     /// parses a part of the tokens
-    fn parse_part(statement: &[&str], global_toks: &mut Vec<&str>) -> BSAst {
-        match statement {
-            ["batch"] => {
-                let mut inner: Vec<&str>;
-                let idx = global_toks.iter().position(|a| (*a).eq("end")).unwrap_or(0);
-                (inner, *global_toks) = {
-                    let (a, b) = global_toks.split_at(idx);
-                    let (_, b) = b.split_first().unwrap();
-                    (Vec::from(a), Vec::from(b))
-                };
-                BSAst::Batch(Self::parse_lines(&mut inner))
-            },
-            ["set", iden, "=", tail @ ..] => {
-                BSAst::SetVar(Box::new(BSAst::Ident(iden.to_string())), Box::new(Self::parse_part(tail, &mut vec![])))
-            },
-            ["gen", tail @ ..] => {
-                let mut args = vec![];
-                let mut unpack_next: bool = false;
-                for token in tail {
-                    if (*token).eq("*") {unpack_next = true;}
-                    else if unpack_next {
-                        args.push(BSAst::Unpack(Box::new(BSAst::Ident(token.to_string()))));
-                        unpack_next = false;
-                    } else {
-                        args.push(BSAst::Ident(token.to_string()));
+    fn parse_part<'a>(statement: &mut Peekable<impl Iterator<Item = &'a BSToken>>, global_toks: &mut (dyn Iterator<Item = &[BSToken]> + '_)) -> BSAst {
+        match statement.peek() {
+            Some(BSToken::Word(t)) if t.eq("batch") => {
+                statement.next();
+                let mut inner = vec![];
+                loop {
+                    match global_toks.next() {
+                        Some([BSToken::Word(s)]) if s.eq("end") => {break},
+                        Some([tokens @ ..]) => inner.append(&mut Vec::from(tokens)),
+                        None => panic!("syntax error"),
                     }
+                }
+                BSAst::Batch(Self::parse_lines(&mut inner.split(|t| *t==BSToken::Break)))
+            },
+            Some(BSToken::Word(t)) if t.eq("set") => {
+                statement.next();
+                let BSToken::Word(iden) = statement.next().expect("Syntax Error") else {panic!("Syntax Error")};
+                if statement.next().expect("Syntax Error").ne(&BSToken::Operator("=".to_string())) {panic!("Syntax Error")}
+                BSAst::SetVar(Box::new(BSAst::Ident((*iden).clone())), Box::new(Self::parse_part(statement, global_toks)))
+            },
+            Some(BSToken::Word(t)) if t.eq("gen") => {
+                statement.next();
+                let mut args = vec![];
+                let tail = statement.collect::<Vec<&BSToken>>();
+                let tail = tail.split(|t| t.eq(&&BSToken::Operator(",".to_string())));
+                for term in tail {
+                    args.push(match term {
+                        [BSToken::Operator(o), tail @ ..] if o=="*" => {
+                            let mut inner = vec![];
+                            for e in tail {inner.push((**e).clone());} 
+                            BSAst::Unpack(Box::new(Self::parse_part(&mut inner.iter().peekable(), global_toks)))
+                        },
+                        [tail @ ..] => {
+                            let mut inner = vec![];
+                            for e in tail {inner.push((**e).clone());} 
+                            Self::parse_part(&mut inner.iter().peekable(), global_toks)
+                        },
+                    });
                 }
                 BSAst::Generate(args)
             },
-            ["[", content @ .., "]"] => {
-                let mut internal = vec![]; 
-                for element in content.split(|s| (*s).eq(",")) {
-                    internal.push(Self::parse_part(element, &mut vec![]));
+            Some(BSToken::Operator(t)) if t.eq("[") => {
+                statement.next();
+                let mut content = vec![];
+                let tail = statement.collect::<Vec<&BSToken>>();
+                let tail = tail.split(|t| t.eq(&&BSToken::Operator(",".to_string())));
+                for term in tail {
+                    let mut inner = vec![];
+                    for e in term {inner.push((**e).clone());} 
+                    content.push(Self::parse_part(&mut inner.iter().peekable(), global_toks))
                 }
-                BSAst::Arr(internal)
+                BSAst::Arr(content)
             },
-            [expr @ ..] 
-            if (expr.contains(&"+") || expr.contains(&"-") || expr.contains(&"*") || expr.contains(&"/") || expr.contains(&"(")) => {
-                Self::parse_expr(expr)
+            Some(_) => {
+                Self::parse_expr(statement)
             },
-            [val] => {Self::parse_expr(&[val])},
-            [] => BSAst::None,
-            _ => panic!("???")
+            _ => BSAst::None
         }
     }
 
     /// parses a math expression
-    fn parse_expr(expr: &[&str]) -> BSAst {
-        let mut expr = expr.iter().peekable();
-        Self::parse_add_expr(&mut expr)
+    fn parse_expr<'a>(expr: &mut Peekable<impl Iterator<Item = &'a BSToken>>) -> BSAst {
+        Self::parse_add_expr(expr)
     }
-    
-    fn parse_add_expr<'a, 'b>(expr: &'a mut Peekable<std::slice::Iter<'b, &str>>) -> BSAst {
+
+    fn parse_add_expr<'a>(expr: &mut Peekable<impl Iterator<Item = &'a BSToken>>) -> BSAst {
         let mut res = Self::parse_mul_expr(expr);
         loop {
             match expr.peek() {
-                Some(&&"+") => {
+                Some(BSToken::Operator(o)) if o == "+" => {
                     expr.next();
                     res = BSAst::ExprAdd(vec![res, Self::parse_mul_expr(expr)], false);
                 },
-                Some(&&"-") => {
+                Some(BSToken::Operator(o)) if o == "-" => {
                     expr.next();
                     res = BSAst::ExprAdd(vec![res, Self::parse_mul_expr(expr)], true);
                 },
@@ -177,15 +178,15 @@ impl BuildParser {
         }
     }
 
-    fn parse_mul_expr<'a, 'b>(expr: &'a mut Peekable<std::slice::Iter<'b, &str>>) -> BSAst {
+    fn parse_mul_expr<'a>(expr: &mut Peekable<impl Iterator<Item = &'a BSToken>>) -> BSAst {
         let mut res = Self::parse_term_expr(expr);
         loop {
             match expr.peek() {
-                Some(&&"*") => {
+                Some(BSToken::Operator(o)) if o == "*" => {
                     expr.next();
                     res = BSAst::ExprMul(vec![res, Self::parse_term_expr(expr)], false);
                 },
-                Some(&&"/") => {
+                Some(BSToken::Operator(o)) if o == "/" => {
                     expr.next();
                     res = BSAst::ExprMul(vec![res, Self::parse_term_expr(expr)], true);
                 },
@@ -194,20 +195,27 @@ impl BuildParser {
         }
     }
 
-    fn parse_term_expr<'a, 'b>(expr: &'a mut Peekable<std::slice::Iter<'b, &str>>) -> BSAst {
+    fn parse_term_expr<'a>(expr: &mut Peekable<impl Iterator<Item = &'a BSToken>>) -> BSAst {
         match expr.peek() {
-            Some(&&"(") => {
+            Some(BSToken::Operator(o)) if o == "(" => {
                 expr.next();
                 let res = Self::parse_add_expr(expr);
-                if expr.next().ne(&Some(&")")) {panic!("Syntax error!")}
+                if expr.next().ne(&Some(&BSToken::Operator(")".to_string()))) {panic!("Syntax error!")}
                 res
             },
-            Some(s) => {
-                if "0123456789".contains((**s).chars().nth(0).unwrap()) {let r = BSAst::Num(s.to_string());expr.next();r}
-                else if (**s).chars().nth(0).unwrap().eq(&'"') {let r = BSAst::Str(s.to_string());expr.next();r}
-                else {let r = BSAst::Ident(s.to_string());expr.next();r}
+            Some(BSToken::Numeric(s)) => {
+                expr.next();
+                BSAst::Num((*s).clone())
             },
-            None => BSAst::None
+            Some(BSToken::String(s)) => {
+                expr.next();
+                BSAst::Str((*s).clone())
+            },
+            Some(BSToken::Word(s)) if !["gen", "set", "batch", "end"].contains(&(s.as_str())) => {
+                expr.next();
+                BSAst::Ident((*s).clone())
+            },
+            _ => BSAst::None
         }
         
     }
